0e17bf981c48fb76f536179ca163195763d09dea
Angel Diaz <angel.diaz@gmail.com>
2025-10-02 12:02:57 +0200

    chore: optimize deployment script by consolidating SCP commands into a single connection for improved efficiency

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,5 @@
 # https://gist.github.com/nepsilon/45fae11f8d173e3370c3
 ssh-add -K ~/.ssh/id_ed25519
 
-# Copy files to the revenuehunt.com server
-scp llms.txt root@143.198.165.23:/var/www/html/llms.txt
-scp shopify.txt root@143.198.165.23:/var/www/html/shopify.txt
-scp shopify-legacy.txt root@143.198.165.23:/var/www/html/shopify-legacy.txt
-scp woocommerce.txt root@143.198.165.23:/var/www/html/woocommerce.txt
+# Copy all files to the revenuehunt.com server in a single connection
+scp llms.txt shopify.txt shopify-legacy.txt woocommerce.txt root@143.198.165.23:/var/www/html/
