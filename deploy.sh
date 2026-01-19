695568b0586da1de9e34f550574dc7a357ef45fc
Angel Diaz <angel.diaz@gmail.com>
2026-01-19 08:35:07 +0100

    chore: remove llms.txt from deployment and update deploy script to exclude it from file transfer

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@
 ssh-add -K ~/.ssh/id_ed25519
 
 # Copy all files to the revenuehunt.com server in a single connection
-scp llms.txt shopify.txt shopify-legacy.txt woocommerce.txt root@143.198.165.23:/var/www/html/
+scp shopify.txt shopify-legacy.txt woocommerce.txt root@143.198.165.23:/var/www/html/
